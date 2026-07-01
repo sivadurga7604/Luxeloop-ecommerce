777dcc22578badfe00cff94a88871370e1fd383d
Sivadurga <sivadurgas7604@gmail.com>
2026-07-01 10:45:27 +0530

    fix: remove duplicate java files

--- ecommerce-frontend/src/main/java/com/ecommerce/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- ecommerce-frontend/src/main/java/com/ecommerce/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
-}
